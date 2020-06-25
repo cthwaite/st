fff22f5b515e23667ffebef46567975917cc38e0
cthwaite <28732700+cthwaite@users.noreply.github.com>
2020-06-25 23:03:25 +0100

    feat(config): Use XC_left_ptr rather than XC_xterm

--- config.h
+++ config.h
@@ -161,7 +161,8 @@ static unsigned int rows = 24;
 /*
  * Default colour and shape of the mouse cursor
  */
-static unsigned int mouseshape = XC_xterm;
+// cursor theme's normal mouse
+static unsigned int mouseshape = XC_left_ptr;
 static unsigned int mousefg = 7;
 static unsigned int mousebg = 0;
 
